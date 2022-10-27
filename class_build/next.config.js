45406f39e14a86d6314fe1724b46cf7a3bfe96b5
aggie97 <4ggie1997@gmail.com>
2022-10-27 10:37:46 +0900

    dockerfile 내용 추가

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: true,
   generateBuildId: () => "codecamp_deploy_09",
+
+  // getServerSideProps 들어있는 페이지 빌드할 때 제외시키기
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
